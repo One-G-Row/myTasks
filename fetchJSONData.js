a6cc7beb53598248d36118e6128ee4b01cda5711
One-G-Row <148204501+One-G-Row@users.noreply.github.com>
2024-01-26 03:49:03 -0800

    fetchJSONData.js

--- fetchJSONData.js
+++ fetchJSONData.js
@@ -1,7 +1,16 @@
 //fetch data from a json file in a local server
 //this should be done using a liveserver otherwise the code will encounter errors on the browser
+//first option
 fetch('./movies.json')
-
 .then((response) => response.json())
-//const revamp = JSON.parse(json)
 .then((json) => console.log(json))
+
+//second option
+fetch('movies.json')
+.then((response)=>{
+console.log('resolved', response)
+return response.json()
+})
+.then((data)=>{
+    console.log(data)
+})
